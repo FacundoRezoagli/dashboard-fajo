30ec9ea4360ef945de988e80e9ae31e840ae1b26
FacundoRezoagli <107441057+FacundoRezoagli@users.noreply.github.com>
2024-04-08 19:45:43 -0300

    a
    a

--- src/app/orders/page.tsx
+++ src/app/orders/page.tsx
@@ -6,8 +6,8 @@ import axios from 'axios';
 import { DataGrid, GridColDef } from '@mui/x-data-grid';
 
 export default function Clients() {
-
   const [dataApi, setDataApi] = useState<any>([]);
+
   useEffect(() => {
     getData();
   }, []);
@@ -33,25 +33,38 @@ export default function Clients() {
     }
   };
 
+  // Función para obtener el nombre del cliente según el ID del cliente
+  const getClientName = async (clientId: number) => {
+    try {
+      const response = await axios.get(`http://localhost:5199/api/clients/${clientId}`);
+      return response.data.companyName;
+    } catch (error) {
+      console.error('Error al obtener el nombre del cliente:');
+      return '';
+    }
+  };
+
   const columns: GridColDef[] = [
     { field: 'id', headerName: '#', width: 100 },
     { field: 'creationDate', headerName: 'Fecha de Creación', width: 200 },
     { field: 'dueDate', headerName: 'Fecha de Vencimiento', width: 200 },
-    { field: 'clientId', headerName: 'ID del Cliente', width: 200 },
+    { field: 'clientId', headerName: 'Nombre del Cliente', width: 200, 
+      // Función para obtener el nombre del cliente y mostrarlo en lugar del ID
+      valueGetter: (params) => getClientName(params) },
   ];
 
   return (
-  <div>
-    <h1 style={{ textAlign: 'center' }}>Pedidos</h1> 
-    <div style={{ height: 900, width: '33%', margin: 'auto' }}>
-      <DataGrid
-        rows={dataApi}
-        columns={columns}
-        pageSizeOptions={[5, 10, 20]}
-        pagination
-        autoHeight
-      />
+    <div>
+      <h1 style={{ textAlign: 'center' }}>Pedidos</h1> 
+      <div style={{ height: 900, width: '60%', margin: 'auto' }}>
+        <DataGrid
+          rows={dataApi}
+          columns={columns}
+          pageSizeOptions={[5, 10, 20]}
+          pagination
+          autoHeight
+        />
+      </div>
     </div>
-  </div>
   );
 }
